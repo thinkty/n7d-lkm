1b925f5e067e3f44a7989cb24262562307e8cd40
thinkty <the.thinkty@gmail.com>
2023-11-08 12:49:54 -0500

    testing

--- n7d_main.c
+++ n7d_main.c
@@ -239,14 +239,14 @@ static enum hrtimer_restart n7d_timer_callback(struct hrtimer * timer)
     static unsigned int bit = -1;
     struct n7d_drvdata * drvdata = container_of(timer, struct n7d_drvdata, timer);
 
-    drvdata->byte = '5';
+    // drvdata->byte = '5';
 
     /* No byte to process, set stop bit and restart timer */
-    // if (drvdata->byte == 0) {
-    //     gpiod_set_value(drvdata->tx, 1);
-    //     hrtimer_forward_now(&drvdata->timer, drvdata->delay);
-    //     return HRTIMER_RESTART;
-    // }
+    if (drvdata->byte == 0) {
+        gpiod_set_value(drvdata->tx, 1);
+        hrtimer_forward_now(&drvdata->timer, drvdata->delay);
+        return HRTIMER_RESTART;
+    }
 
     /* Start bit */
     if (bit == -1) {
